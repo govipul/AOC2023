1ef827b0a7260b3502be8c1843a64b040dc98fd5
Vipul Goswami <vipul.goswami@schibsted.com@A120889-NO.home>
2023-12-01 21:26:01 +0100

    Day1 Code improvement

--- day1/calibrationvalue/src/main/kotlin/service/CalibrateValue.kt
+++ day1/calibrationvalue/src/main/kotlin/service/CalibrateValue.kt
@@ -27,13 +27,13 @@ class CalibrateValue {
 
     fun getStringToDigit(input: String): List<NumberPos> {
         val result  = mutableListOf<NumberPos>()
-        var startIndex = 0
-        while (startIndex < input.length-1) {
-            val data = input.findAnyOf(stringToDigit.keys, startIndex = startIndex, ignoreCase = true)
+        var searchIndex = 0
+        while (searchIndex < input.length-1) {
+            val data = input.findAnyOf(stringToDigit.keys, startIndex = searchIndex, ignoreCase = true)
             if (data != null) {
                 val number = stringToDigit[data.second]
                 number?.let { NumberPos(number = it, pos = data.first) }?.let { result.add(it) }
-                startIndex = data.first-1 + data.second.length-1
+                searchIndex = data.first-1 + data.second.length-1
             } else {
                 break
             }
